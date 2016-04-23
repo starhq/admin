86bffa7609c2604944c6e476381a5ab30fe92007
starhq <50849806@qq.com>
2016-04-24 00:53:02 +0800

    dashboard2 is ok

--- src/app/main/dashboard2/dashboard2.controller.js
+++ src/app/main/dashboard2/dashboard2.controller.js
@@ -8,78 +8,184 @@
     /** @ngInject */
     function RegistController() {
 
+        var randomColorFactor = function() {
+            return Math.round(Math.random() * 255);
+        };
+        var randomColor = function(opacity) {
+            return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',' + (opacity || '.3') + ')';
+        };
+
         function generateSalesChart() {
             var salesChartData = {
                 labels: ["January", "February", "March", "April", "May", "June", "July"],
                 datasets: [{
                     label: "Electronics",
-                    backgroundColor: "rgb(210, 214, 222)",
-                    borderColor: "rgb(210, 214, 222)",
-                    pointBackgroundColor: "rgb(210, 214, 222)",
-                    pointBorderColor: "#c1c7d1",
-                    pointHoverBackgroundColor: "#fff",
-                    pointHoverBorderColor: "rgb(220,220,220)",
                     data: [65, 59, 80, 81, 56, 55, 40]
                 }, {
                     label: "Digital Goods",
-                    backgroundColor: "rgba(60,141,188,0.9)",
-                    borderColor: "rgba(60,141,188,0.8)",
-                    pointBackgroundColor: "#3b8bba",
-                    pointBorderColor: "rgba(60,141,188,1)",
-                    pointHoverBackgroundColor: "#fff",
-                    pointHoverBorderColor: "rgba(60,141,188,1)",
                     data: [28, 48, 40, 19, 86, 27, 90]
                 }]
             };
 
-            var salesChartOptions = {
-                //Boolean - If we should show the scale at all
-                showScale: true,
-                //Boolean - Whether grid lines are shown across the chart
-                scaleShowGridLines: false,
-                //String - Colour of the grid lines
-                scaleGridLineColor: "rgba(0,0,0,.05)",
-                //Number - Width of the grid lines
-                scaleGridLineWidth: 1,
-                //Boolean - Whether to show horizontal lines (except X axis)
-                scaleShowHorizontalLines: true,
-                //Boolean - Whether to show vertical lines (except Y axis)
-                scaleShowVerticalLines: true,
-                //Boolean - Whether the line is curved between points
-                bezierCurve: true,
-                //Number - Tension of the bezier curve between points
-                bezierCurveTension: 0.3,
-                //Boolean - Whether to show a dot for each point
-                pointDot: false,
-                //Number - Radius of each point dot in pixels
-                pointDotRadius: 4,
-                //Number - Pixel width of point dot stroke
-                pointDotStrokeWidth: 1,
-                //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-                pointHitDetectionRadius: 20,
-                //Boolean - Whether to show a stroke for datasets
-                datasetStroke: true,
-                //Number - Pixel width of dataset stroke
-                datasetStrokeWidth: 2,
-                //Boolean - Whether to fill the dataset with a color
-                datasetFill: true,
-                //String - A legend template
-                legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%=datasets[i].label%></li><%}%></ul>",
-                //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-                maintainAspectRatio: true,
-                //Boolean - whether to make the chart responsive to window resizing
-                responsive: true
+            angular.forEach(salesChartData.datasets, function(dataset) {
+                var color = randomColor(0.4);
+                dataset.borderColor = color;
+                dataset.backgroundColor = color;
+                dataset.pointBorderColor = color;
+                dataset.pointBackgroundColor = color;
+                dataset.pointBorderWidth = 1;
+            });
+
+            var config = {
+                responsive: true,
+                tooltips: {
+                    mode: 'label'
+                },
+                legend: {
+                    display: false
+                },
+                hover: {
+                    mode: 'label'
+                },
+                scales: {
+                    xAxes: [{
+                        // gridLines: {
+                        //     display: false
+                        // },
+                        display: true,
+                        scaleLabel: {
+                            show: true,
+                            labelString: 'Month'
+                        }
+                    }],
+                    yAxes: [{
+                        // gridLines: {
+                        //     display: false
+                        // },
+                        display: true,
+                        scaleLabel: {
+                            show: true,
+                            labelString: 'value'
+                        }
+                    }]
+                }
             };
 
-            var salesChartCanvas = angular.element("#salesChart").get(0).getContext("2d");
+            var ctx = angular.element('#salesChart').get(0).getContext("2d");
 
-            new Chart(salesChartCanvas, {
+            new Chart(ctx, {
                 type: 'line',
                 data: salesChartData,
-                options: salesChartOptions
+                options: config
             });
         }
 
         generateSalesChart();
+
+
+        function fillMap() {
+            angular.element('#world-map-markers').vectorMap({
+                map: 'world_mill_en',
+                normalizeFunction: 'polynomial',
+                hoverOpacity: 0.7,
+                hoverColor: false,
+                backgroundColor: 'transparent',
+                regionStyle: {
+                    initial: {
+                        fill: 'rgba(210, 214, 222, 1)',
+                        "fill-opacity": 1,
+                        stroke: 'none',
+                        "stroke-width": 0,
+                        "stroke-opacity": 1
+                    },
+                    hover: {
+                        "fill-opacity": 0.7,
+                        cursor: 'pointer'
+                    },
+                    selected: {
+                        fill: 'yellow'
+                    },
+                    selectedHover: {}
+                },
+                markerStyle: {
+                    initial: {
+                        fill: '#00a65a',
+                        stroke: '#111'
+                    }
+                },
+                markers: [
+                    { latLng: [41.90, 12.45], name: 'Vatican City' },
+                    { latLng: [43.73, 7.41], name: 'Monaco' },
+                    { latLng: [-0.52, 166.93], name: 'Nauru' },
+                    { latLng: [-8.51, 179.21], name: 'Tuvalu' },
+                    { latLng: [43.93, 12.46], name: 'San Marino' },
+                    { latLng: [47.14, 9.52], name: 'Liechtenstein' },
+                    { latLng: [7.11, 171.06], name: 'Marshall Islands' },
+                    { latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis' },
+                    { latLng: [3.2, 73.22], name: 'Maldives' },
+                    { latLng: [35.88, 14.5], name: 'Malta' },
+                    { latLng: [12.05, -61.75], name: 'Grenada' },
+                    { latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines' },
+                    { latLng: [13.16, -59.55], name: 'Barbados' },
+                    { latLng: [17.11, -61.85], name: 'Antigua and Barbuda' },
+                    { latLng: [-4.61, 55.45], name: 'Seychelles' },
+                    { latLng: [7.35, 134.46], name: 'Palau' },
+                    { latLng: [42.5, 1.51], name: 'Andorra' },
+                    { latLng: [14.01, -60.98], name: 'Saint Lucia' },
+                    { latLng: [6.91, 158.18], name: 'Federated States of Micronesia' },
+                    { latLng: [1.3, 103.8], name: 'Singapore' },
+                    { latLng: [1.46, 173.03], name: 'Kiribati' },
+                    { latLng: [-21.13, -175.2], name: 'Tonga' },
+                    { latLng: [15.3, -61.38], name: 'Dominica' },
+                    { latLng: [-20.2, 57.5], name: 'Mauritius' },
+                    { latLng: [26.02, 50.55], name: 'Bahrain' },
+                    { latLng: [0.33, 6.73], name: 'São Tomé and Príncipe' }
+                ]
+            });
+        }
+
+        fillMap();
+
+        function initSpark() {
+            angular.forEach(angular.element('.sparkbar'), function(bar) {
+                var ele = angular.element(bar);
+                ele.sparkline('html', {
+                    type: 'bar',
+                    height: ele.data('height') ? ele.data('height') : '30',
+                    barColor: ele.data('color')
+                });
+            });
+
+        }
+
+        initSpark();
+
+        function fillBar() {
+            var data = {
+
+                labels: ["Chrome", "IE", "FireFox", "Safari", "Opera", "Navigator"],
+                datasets: [{
+                    data: [700, 500, 400, 600, 300, 100],
+                    backgroundColor: ["#f56954", "#00a65a", "#f39c12", "#00c0ef", "#3c8dbc", "#d2d6de"],
+                    hoverBackgroundColor: ["#f56954", "#00a65a", "#f39c12", "#00c0ef", "#3c8dbc", "#d2d6de"]
+                }]
+            };
+
+            var options = {
+                legend: {
+                    display: false
+                }
+            };
+
+            var ctx = angular.element('#pieChart').get(0).getContext("2d");
+
+            new Chart(ctx, {
+                type: 'doughnut',
+                data: data,
+                options: options
+            });
+        }
+
+        fillBar();
     }
 })();
