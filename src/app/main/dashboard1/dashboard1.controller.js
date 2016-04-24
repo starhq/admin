cbdcfffeb67f1ecd6b0e367e719cd77158a0c573
starhq <50849806@qq.com>
2016-04-24 17:37:28 +0800

    dashboard1 sortabe is ok.tabset chang is processing.not complete

--- src/app/main/dashboard1/dashboard1.controller.js
+++ src/app/main/dashboard1/dashboard1.controller.js
@@ -3,8 +3,31 @@
 
     angular
         .module('admin')
-        .controller('Dashboard1Controller', RegistController);
+        .controller('Dashboard1Controller', DashBoardController);
 
     /** @ngInject */
-    function RegistController() {}
+    function DashBoardController($scope) {
+
+        var vm = this;
+
+        $scope.$watch('vm.activeTabDate', function(newValue, oldValue) {
+            if (newValue !== oldValue) {
+                console.log('--' + newValue);
+            }
+        });
+
+
+        function initSortable() {
+            angular.element(".connectedSortable").sortable({
+                placeholder: "sort-highlight",
+                connectWith: ".connectedSortable",
+                handle: ".box-header, .nav-tabs",
+                forcePlaceholderSize: true,
+                zIndex: 999999
+            });
+            angular.element(".connectedSortable .box-header, .connectedSortable .nav-tabs-custom").css("cursor", "move");
+        }
+
+        initSortable();
+    }
 })();
