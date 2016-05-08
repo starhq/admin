46621873c443ebb6dfedfdbdc7652eab738a51ad
starhq <50849806@qq.com>
2016-05-08 19:41:20 +0800

    remove angular-flot

--- src/app/main/flot/flot.controller.js
+++ src/app/main/flot/flot.controller.js
@@ -10,16 +10,5 @@
         var vm = this;
         vm.update = function(data) {}
 
-        vm.dataset = [{ data: [], yaxis: 1, label: 'sin' }];
-        vm.options = {
-            legend: {
-                container: '#legend',
-                show: true
-            }
-        };
-
-        for (var i = 0; i < 14; i += 0.5) {
-            vm.dataset[0].data.push([i, Math.sin(i)]);
-        }
     }
 })();
