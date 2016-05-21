0278ca578b7d9c59c29d73850d2cf16f971b898c
starhq <50849806@qq.com>
2016-05-21 21:28:41 +0800

    advance is ok,but date component is some space to improve

--- src/app/main/advanced/advanced.controller.js
+++ src/app/main/advanced/advanced.controller.js
@@ -1,3 +1,4 @@
+/* global moment */
 (function() {
     'use strict';
 
@@ -11,6 +12,59 @@
         // var vm = this;
         angular.element(".select2").select2();
 
+        angular.element("[data-mask]").inputmask();
 
+        angular.element(".my-colorpicker1").colorpicker();
+        angular.element(".my-colorpicker2").colorpicker();
+
+        angular.element(".timepicker").timepicker({
+            showInputs: false
+        });
+
+        angular.element('#datepicker').datepicker({
+            autoclose: true
+        });
+
+        angular.element('#reservation').daterangepicker();
+
+        angular.element('#reservationtime').daterangepicker({
+            timePicker: true,
+            timePickerIncrement: 30,
+            locale: {
+                format: 'MM/DD/YYYY h:mm A'
+            }
+        });
+
+        angular.element('#daterange-btn').daterangepicker({
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                },
+                startDate: moment().subtract(29, 'days'),
+                endDate: moment()
+            },
+            function(start, end) {
+                angular.element('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+            }
+        );
+
+        angular.element('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+            checkboxClass: 'icheckbox_minimal-blue',
+            radioClass: 'iradio_minimal-blue'
+        });
+        //Red color scheme for iCheck
+        angular.element('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+            checkboxClass: 'icheckbox_minimal-red',
+            radioClass: 'iradio_minimal-red'
+        });
+        //Flat red color scheme for iCheck
+        angular.element('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+            checkboxClass: 'icheckbox_flat-green',
+            radioClass: 'iradio_flat-green'
+        });
     }
 })();
